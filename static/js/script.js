5b5172e296956f813bb8257ee9ec242118e5fff6
Sumit Kumar Sharma <124140248+SumitSharmaSankhyan@users.noreply.github.com>
2026-06-25 17:29:05 +0530

    Implement file upload functionality with drag and drop

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,126 @@
-console.log("Invoice AI Extractor Started");
+const chooseBtn=document.getElementById("chooseBtn");
+
+const pdfInput=document.getElementById("pdfInput");
+
+const fileList=document.getElementById("fileList");
+
+const uploadBtn=document.getElementById("uploadBtn");
+
+const status=document.getElementById("status");
+
+const dropZone=document.getElementById("dropZone");
+
+let selectedFiles=[];
+
+
+chooseBtn.onclick=()=>{
+
+pdfInput.click();
+
+};
+
+
+pdfInput.onchange=()=>{
+
+selectedFiles=[...pdfInput.files];
+
+displayFiles();
+
+};
+
+
+dropZone.addEventListener("dragover",(e)=>{
+
+e.preventDefault();
+
+dropZone.style.background="#dff4ff";
+
+});
+
+
+dropZone.addEventListener("dragleave",()=>{
+
+dropZone.style.background="white";
+
+});
+
+
+dropZone.addEventListener("drop",(e)=>{
+
+e.preventDefault();
+
+dropZone.style.background="white";
+
+selectedFiles=[...e.dataTransfer.files];
+
+displayFiles();
+
+});
+
+
+function displayFiles(){
+
+fileList.innerHTML="";
+
+selectedFiles.forEach(file=>{
+
+fileList.innerHTML+=`
+
+<div class="file-item">
+
+📄 ${file.name}
+
+</div>
+
+`;
+
+});
+
+}
+
+
+uploadBtn.onclick=()=>{
+
+if(selectedFiles.length==0){
+
+alert("Select PDF files");
+
+return;
+
+}
+
+const formData=new FormData();
+
+selectedFiles.forEach(file=>{
+
+formData.append("files",file);
+
+});
+
+status.innerHTML="Uploading...";
+
+fetch("/upload",{
+
+method:"POST",
+
+body:formData
+
+})
+
+.then(res=>res.json())
+
+.then(data=>{
+
+status.innerHTML="✅ Uploaded Successfully";
+
+console.log(data);
+
+})
+
+.catch(()=>{
+
+status.innerHTML="Upload Failed";
+
+});
+
+};
